24b456212a33f4eb7fd019047f600abf5247415b
denisg <denisg.santiment.net>
2022-09-27 22:04:43 +0200

    Clickhouse RDD
    * Lazy loading
    * SQL Execution moved to Spark worker
    
    ps-id: 62a497a8-3255-42bd-af3e-ced61213a57c

--- src/main/scala/com/blackmorse/spark/clickhouse/reader/ReaderClickhouseRelation.scala
+++ src/main/scala/com/blackmorse/spark/clickhouse/reader/ReaderClickhouseRelation.scala
@@ -1,17 +1,15 @@
 package com.blackmorse.spark.clickhouse.reader
 
 import com.blackmorse.spark.clickhouse.utils.JDBCTimeZoneUtils
-import com.blackmorse.spark.clickhouse.writer.ClickhouseTimeZoneInfo
 import com.blackmorse.spark.clickhouse.{CLICKHOUSE_HOST_NAME, CLICKHOUSE_PORT, TABLE}
 import com.clickhouse.jdbc.ClickHouseDriver
+import org.apache.spark.ClickhouseRdd
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.sources.{BaseRelation, TableScan}
 import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.{Row, SQLContext}
 
 import java.util.Properties
-import scala.collection.mutable
-import scala.util.{Failure, Success, Using}
 
 case class ReaderClickhouseRelation(@transient sqlContext: SQLContext, parameters: Map[String, String])
   extends BaseRelation with TableScan with Serializable {
@@ -32,24 +30,15 @@ case class ReaderClickhouseRelation(@transient sqlContext: SQLContext, parameter
   override def buildScan(): RDD[Row] = {
     //TODO scale
     val clickhouseTimeZoneInfo = JDBCTimeZoneUtils.fetchClickhouseTimeZoneFromServer(url)
-
-    sqlContext.sparkContext.parallelize(Seq(()), 1)
-      .flatMap(_ => {
-        val selectSQL = s"SELECT ${fieldsNames} FROM $table"
-        Using(new ClickHouseDriver().connect(url, new Properties))(connection => {
-          Using(connection.createStatement()) { stmt =>
-            val rs = stmt.executeQuery(selectSQL)
-            val rows = mutable.Buffer[Row]()
-            while (rs.next()) {
-              val fieldValues = clickhouseFields.map(_.extractFromRs(rs)(clickhouseTimeZoneInfo))
-              rows += Row.fromSeq(fieldValues)
-            }
-            rows
-          }
-        })
-      }.flatten match {
-        case Success(rows) => rows
-        case Failure(e) => throw e
+    new ClickhouseRdd[Row](sqlContext.sparkContext,
+      () => new ClickHouseDriver().connect(url, new Properties()),
+      s"SELECT ${fieldsNames} FROM $table",
+      rs => {
+        val fieldValues = clickhouseFields.map(_.extractFromRs(rs)(clickhouseTimeZoneInfo))
+        Row.fromSeq(fieldValues)
       })
   }
 }
+
+
+
--- src/main/scala/org/apache/spark/ClickhouseRdd.scala
+++ src/main/scala/org/apache/spark/ClickhouseRdd.scala
@@ -6,32 +6,27 @@ import org.apache.spark.util.NextIterator
 import java.sql.{Connection, ResultSet}
 import scala.reflect.ClassTag
 
-private case class ClickhousePartition(idx: Int, val lower: Long, val upper: Long) extends Partition {
-  override def index: Int = idx
+object NotImplementedPartitioning extends Partition {
+  override def index: Int = 0
 }
 
 class ClickhouseRdd[T: ClassTag](
                                   sc: SparkContext,
                                   getConnection: () => Connection,
                                   sql: String,
-                                  lowerBound: Long,
-                                  upperBound: Long,
-                                  numPartitions: Int,
                                   mapRow: ResultSet => T)
   extends RDD[T](sc, Nil) {
-  override def compute(split: Partition, context: TaskContext): Iterator[T] = new NextIterator[T]
+
+  override def compute(partition: Partition, context: TaskContext): Iterator[T] = new NextIterator[T]
   {
 
     context.addTaskCompletionListener[Unit]{ _ => closeIfNeeded() }
-    val part = split.asInstanceOf[ClickhousePartition]
     val conn = getConnection()
     val stmt = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
 
     stmt.setFetchSize(300)
     logInfo(s"statement fetch size set to: ${stmt.getFetchSize}")
 
-    stmt.setLong(1, part.lower)
-    stmt.setLong(2, part.upper)
     val rs = stmt.executeQuery()
 
     override def getNext(): T = {
@@ -69,13 +64,5 @@ class ClickhouseRdd[T: ClassTag](
     }
   }
 
-  override protected def getPartitions: Array[Partition] = {
-    // bounds are inclusive, hence the + 1 here and - 1 on end
-    val length = BigInt(1) + upperBound - lowerBound
-    (0 until numPartitions).map { i =>
-      val start = lowerBound + ((i * length) / numPartitions)
-      val end = lowerBound + (((i + 1) * length) / numPartitions) - 1
-      new ClickhousePartition(i, start.toLong, end.toLong)
-    }.toArray
-  }
+  override protected def getPartitions: Array[Partition] = Array(NotImplementedPartitioning)
 }

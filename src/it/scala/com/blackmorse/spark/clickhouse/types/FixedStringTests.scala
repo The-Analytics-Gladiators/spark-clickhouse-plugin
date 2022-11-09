971eab235bda54cc3b34c4ed42cd5d3db1baba5c
Vlady <Averianov87@gmail.com>
2022-11-09 14:52:06 +0300

    SC-63 - add some tests

--- src/it/scala/com/blackmorse/spark/clickhouse/types/FixedStringTests.scala
+++ src/it/scala/com/blackmorse/spark/clickhouse/types/FixedStringTests.scala
@@ -8,10 +8,28 @@ import org.scalatest.flatspec.AnyFlatSpec
 class FixedStringTests extends AnyFlatSpec with DataFrameSuiteBase {
   import sqlContext.implicits._
 
-  "FixedString" should "be supported" in {
+  "FixedString(3)" should "be supported" in {
     testPrimitiveAndArray(ClickhouseFixedString(nullable = false, lowCardinality = false, length = 3))(
       cases = Seq(
         (111 to 222) map (_.toString)
+      ),
+      rowConverter = row => row.getString(0)
+    )
+  }
+
+  "FixedString(1)" should "be supported" in {
+    testPrimitiveAndArray(ClickhouseFixedString(nullable = false, lowCardinality = false, length = 1))(
+      cases = Seq(
+        (1 to 9) map (_.toString)
+      ),
+      rowConverter = row => row.getString(0)
+    )
+  }
+
+  "FixedString(16)" should "be supported" in {
+    testPrimitiveAndArray(ClickhouseFixedString(nullable = false, lowCardinality = false, length = 36))(
+      cases = Seq(
+        List.fill(100)(java.util.UUID.randomUUID.toString)
       ),
       rowConverter = row => row.getString(0)
     )

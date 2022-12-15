c6f9ce5c8b7499c7c165a799bb6d7903464e3502
Vlady <Averianov87@gmail.com>
2022-12-15 16:17:59 +0300

    SC-66 - add tests for Dates Backward Compatibility

--- src/it/scala/com/blackmorse/spark/clickhouse/write/DatesBackwardCompatibilityTest.scala
+++ src/it/scala/com/blackmorse/spark/clickhouse/write/DatesBackwardCompatibilityTest.scala
@@ -1,57 +1,40 @@
 package com.blackmorse.spark.clickhouse.write
 
-import com.blackmorse.spark.clickhouse.ClickhouseHosts._
-import com.blackmorse.spark.clickhouse.ClickhouseTests.withTable
+import com.blackmorse.spark.clickhouse.sql.types.ClickhouseDateTime
+import com.blackmorse.spark.clickhouse.sql.types.primitives.ClickhouseDate
+import com.blackmorse.spark.clickhouse.types.BaseTestCases.testPrimitiveAndArray
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{DateType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.types.{DateType, TimestampType}
 import org.scalatest.flatspec.AnyFlatSpec
 
 import java.sql.{Date, Timestamp}
-import scala.collection.immutable.Seq
+import java.time.{LocalDate, LocalDateTime}
 
 
 class DatesBackwardCompatibilityTest extends AnyFlatSpec with DataFrameSuiteBase {
 
-  import com.blackmorse.spark.clickhouse._
+  import sqlContext.implicits._
+  implicit val ord: Ordering[Date] = Ordering.by(_.getTime)
+  implicit val timestampOrdering: Ordering[Timestamp] = Ordering.by(_.getTime)
 
-  private val nowTimestamp: Long = System.currentTimeMillis()
+  private lazy val nowTimestamp: Long = System.currentTimeMillis()
 
   "Date" should "write in ch DateTime column" in {
-    withTable(Seq("a DateTime"), "a") {
-      val storedDate = new Date(nowTimestamp)
-      val expectedDate = storedDate.toLocalDate.toString + " 00:00:00.0"
-
-      val schema = StructType(Seq(StructField("a", DateType, nullable = true)))
-      val data = Seq(Row(storedDate))
-      val df = sqlContext.createDataFrame(sc.parallelize(data), schema)
-
-      df.write.clickhouse(shard1Replica1.hostName, shard1Replica1.port, testTable)
-
-      val resDf = sqlContext.read.clickhouse(shard1Replica1.hostName, shard1Replica1.port, testTable)
-
-      val result = resDf.collect().map(_.getTimestamp(0)).map(_.toString)
-
-      assert(result sameElements Array(expectedDate))
-    }
+    testPrimitiveAndArray(ClickhouseDateTime(nullable = false, lowCardinality = false))(
+      cases = Seq(Seq(new Date(nowTimestamp))),
+      rowConverter = row => row.getTimestamp(0),
+      forceSparkType = DateType,
+      convertToOriginalType = t => Timestamp.valueOf(t.toString + " 00:00:00.0")
+    )
   }
 
   "DateTime" should "write in ch date column" in {
-    withTable(Seq("a date"), "a") {
-      val storedDate = new Timestamp(nowTimestamp)
-      val expectedDate = storedDate.toLocalDateTime.toLocalDate.toString
-      val schema = StructType(Seq(StructField("a", TimestampType, nullable = true)))
-      val data = Seq(Row(storedDate))
-      val df = sqlContext.createDataFrame(sc.parallelize(data), schema)
-
-      df.write.clickhouse(shard1Replica1.hostName, shard1Replica1.port, testTable)
-
-      val resDf = sqlContext.read.clickhouse(shard1Replica1.hostName, shard1Replica1.port, testTable)
-
-      val result = resDf.collect().map(_.getDate(0)).map(_.toString)
-
-      assert(result sameElements Array(expectedDate))
-    }
+    testPrimitiveAndArray(ClickhouseDate(nullable = false, lowCardinality = false))(
+      cases = Seq(Seq(new Timestamp(nowTimestamp))),
+      rowConverter = row => row.getDate(0),
+      forceSparkType = TimestampType,
+      convertToOriginalType = _ => Date.valueOf(LocalDate.now())
+    )
   }
 
 }

e3321f5358395986babbdedd4f57549a7553a6ba
Andrei.Kochemirovskii <blackmorse@live.com>
2022-12-27 23:40:21 +0400

    All tests

--- debug_tests.sh
+++ debug_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # In IDEA: Debug Remote JVM, port 5005, attach to remote JVM. Params: -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
-docker build -t sbt_ch_plugin:latest . && docker rm sbt-clickhouse-plugin && docker run -it -p 5005:5005  --name sbt-clickhouse-plugin --network ch_network sbt_ch_plugin:latest sbt  it:"testOnly *ShardedWriterTests"
+docker build -t sbt_ch_plugin:latest . && docker rm sbt-clickhouse-plugin && docker run -it -p 5005:5005  --name sbt-clickhouse-plugin --network ch_network sbt_ch_plugin:latest sbt test it:test
